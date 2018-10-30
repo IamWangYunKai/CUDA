7617a2ca28eb8f6c8943461ddf0584a7e42255a6
IamWangYunKai <615349118@163.com>
2018-10-31 00:38:34 +0800

    open mp test

--- OpenMP/test1/src/main.cpp
+++ OpenMP/test1/src/main.cpp
@@ -1,17 +1,52 @@
 #include <iostream>
-//#include <omp.h>
+#include <omp.h>
+
 using namespace std;
 
 int main(){
 
-# ifdef _OPENMP
-	cout << "Compiled by an OpenMP compliant implementation." << endl;
+#ifndef _OPENMP
+	cout << "OpenMP not supporte !" << endl;
 # endif
 
-#pragma omp parallel for
+	float startTime = omp_get_wtime();
+
+// use parallel for loop
+#pragma omp parallel for num_threads(8)
 	for (int i = 0; i < 8; i++){
-		cout << "This is " << i << endl;
+		cout << "This is thread: " << omp_get_thread_num() << endl;
 	}
+
+	float endTime = omp_get_wtime();
+	cout << "Used time: " << endTime - startTime <<endl;
+
+// use reductions, operator can be + - * & | ^ && ||
+	const int size = 100;
+	double array[size] = {0};
+	double sum = 0;
+#pragma omp parallel for reduction(+:sum)
+	for (int i = 0; i < size; i++) {
+	    sum += array[i];
+	}
+
+/*
+"#pragma omp parallel" will parallelly run the code in parallel region.
+If your CPU's max threads number is 8, then the code will run 8 times. 
+"private(tid, nthreads)" will copy data from public to private.
+*/
+	int tid, nthreads;
+#pragma omp parallel private(tid, nthreads)
+{
+	tid = omp_get_thread_num();
+	cout << "This is thread: " << tid << endl;
+// Synchronize all threads
+#pragma omp barrier
+	if (0 == tid){
+		nthreads = omp_get_num_threads();
+		cout << "Number of threads: " << nthreads << endl;
+	}
+}
+
 	getchar();
 	return 0;
 }
